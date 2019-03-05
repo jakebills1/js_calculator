b11df67ac18c61d49db400f979ac0b6b09c7bde0
Jake Bills <jakebills1@gmail.com>
2019-03-05 16:32:05 -0700

    basic function

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 //variables
+let leftHand = [];
+let rightHand = [];
+let operator;
+let operatorPushed = false;
 var seven = document.getElementById("seven")
 var eight = document.getElementById("eight")
 var nine = document.getElementById("nine")
@@ -13,52 +17,104 @@ var divide = document.getElementById("divide")
 var times = document.getElementById("times")
 var minus = document.getElementById("minus")
 var plus = document.getElementById("plus")
+var equals = document.getElementById("equals")
 
 var display = document.getElementById("screen")
 //event listeners
 seven.addEventListener("click", function(){
+  checkIfOperatorPushed(7);
   addToDisplay(seven.innerHTML);
 })
 eight.addEventListener("click", function(){
+  checkIfOperatorPushed(8);
   addToDisplay(eight.innerHTML);
 })
 nine.addEventListener("click", function(){
+  checkIfOperatorPushed(9);
   addToDisplay(nine.innerHTML);
 })
 four.addEventListener("click", function(){
+  checkIfOperatorPushed(4);
   addToDisplay(four.innerHTML);
 })
 five.addEventListener("click", function(){
+  checkIfOperatorPushed(5);
   addToDisplay(five.innerHTML);
 })
 six.addEventListener("click", function(){
+  checkIfOperatorPushed(6);
   addToDisplay(six.innerHTML);
 })
 one.addEventListener("click", function(){
+  checkIfOperatorPushed(1);
   addToDisplay(one.innerHTML);
 })
 two.addEventListener("click", function(){
+  checkIfOperatorPushed(2);
   addToDisplay(two.innerHTML);
 })
 three.addEventListener("click", function(){
+  checkIfOperatorPushed(3);
   addToDisplay(three.innerHTML);
 })
 zero.addEventListener("click", function(){
+  checkIfOperatorPushed(0);
   addToDisplay(zero.innerHTML);
 })
 divide.addEventListener("click", function(){
+  operator = "/"
+  operatorPushed = true;
   addToDisplay("&divide");
 })
 times.addEventListener("click", function(){
+  operator = "*"
+  operatorPushed = true;
   addToDisplay("&times");
 })
 minus.addEventListener("click", function(){
+  operator = "-"
+  operatorPushed = true;
   addToDisplay("-");
 })
 plus.addEventListener("click", function(){
+  operator = "+"
+  operatorPushed = true;
   addToDisplay("+");
 })
+equals.addEventListener("click", function(){
+  displayAnswer()
+})
+
 //functions
 function addToDisplay(text){
   display.innerHTML += text
+}
+
+function checkIfOperatorPushed(num){
+  if (!operatorPushed){
+    leftHand.push(num);
+  } else {
+    rightHand.push(num);
+  }
+}
+
+function displayAnswer(){
+  var answer;
+  switch (operator){
+    case "/":
+      answer = Number(leftHand.join("")) / Number(rightHand.join(""));
+      break;
+    case "*":
+      answer = Number(leftHand.join("")) * Number(rightHand.join(""));
+      break;
+    case "-":
+      answer = Number(leftHand.join("")) - Number(rightHand.join(""));
+      break;
+    case "+":
+      answer = Number(leftHand.join("")) + Number(rightHand.join(""));
+      break;
+    default:
+      answer = "Invalid Expression";
+  }
+  display.innerHTML = answer
 }
